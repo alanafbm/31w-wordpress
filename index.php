fd77220eb8de9b7d8d29c56f463b1c166042800a
alanafbm <alanaaaa@hotmail.com>
2022-05-30 17:59:35 -0400

    s3-c3-AM

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package mon_31w
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 <h1>Mon premier modèle Wordpress</h1>
-
+<?php
+    if(have_posts()):
+        while(get_post()) : the_post(); ?>
+      <p><?php the_title() ?></p>
+<?php endwhile; ?>
+<?php endif; ?>
 <?php get_footer(); ?>
