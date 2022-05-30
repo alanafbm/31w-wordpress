569c8d4f17d6a42d480fa369a828cc44c719a97a
alanafbm <alanaaaa@hotmail.com>
2022-05-30 16:58:29 -0400

    changements header et footer

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
-<?php wp_footer(); ?>
+
+    <footer class="site__footer">
+        Footer
+    </footer>
+    <?php wp_footer(); ?>
+    </section> <!--  fin .site -->
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,26 @@
-<?php wp_head(); ?>
+<?php
+    /**
+     * Template header
+     * Template integré dans tous les modeles de thème
+     * @link : https://developer.wordpress.org/themes
+     * @package : 31w
+     */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+</head>
+<body>
+    <section class="site">
+        <header class="site__header">
+            <section class="site__branding">
+Titre do site
+            </section>
+            <nav class="site__navigation">Menu</nav>
+        </header>
+   
