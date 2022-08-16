1586ba010c3ba6a3efd7f7c0c985ad62551c31ce
Alana Moraes <86125223+alanafbm@users.noreply.github.com>
2022-08-16 16:46:17 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,22 +1,21 @@
-
 <?php get_header(); ?> 
-
-<h1>front-page.php Mon premier modèle Wordpress</h1>
-
-<?php
-if (have_posts()):
-    while(have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php $lien = " ... <a class='bouton' href='" 
-                . get_permalink()  
-                . "'>"
-                . substr(get_the_title(),0,8) 
-                ."</a>"; 
-        ?>
-
-<p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
-
-    <?php endwhile; ?>
-    <?php endif; ?>    
-
+<section class="site__main flex-column">
+      <h1>Cours au college maisonneuve</h1>
+  <div class="flex-row">
+      <?php
+      if (have_posts()):
+          while(have_posts()) : the_post(); ?>
+          
+              <div class="card"><h2><a href="<?= get_permalink() ?>"> <?php the_field('titre'); ?></a></h2>
+              <?php $image = get_field('image'); if( !empty( $image ) ): ?>
+                  <img height="400" width="250" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+              <?php endif; ?>    
+              <h3><?php the_field('sous_titre'); ?></h3>
+              <p><?php the_field('resume'); ?></p>
+              <a href="<?= get_permalink() ?>" class='bouton' style="text-align:center; font-weight:bold; font-size:1.2em">Voir plus</a></div>  
+         
+          <?php endwhile; ?>
+      <?php endif; ?>  
+  </div>  
+</section>
 <?php get_footer(); ?>
