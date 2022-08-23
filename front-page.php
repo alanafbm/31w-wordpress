d5e16ee421490c5f2137b4670d66f7bf35314c5e
Alana Moraes <86125223+alanafbm@users.noreply.github.com>
2022-08-23 16:19:28 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,21 +1,28 @@
 <?php get_header(); ?> 
 <section class="site__main flex-column">
-      <h1>Cours au college maisonneuve</h1>
+      <h1 class="align-center">Cours</h1>
   <div class="flex-row">
       <?php
       if (have_posts()):
           while(have_posts()) : the_post(); ?>
           
-              <div class="card"><h2><a href="<?= get_permalink() ?>"> <?php the_field('titre'); ?></a></h2>
+              <div class="card"><h2 class="align-center"><a href="<?= get_permalink() ?>"> <?php the_field('titre'); ?></a></h2>
               <?php $image = get_field('image'); if( !empty( $image ) ): ?>
                   <img height="400" width="250" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
               <?php endif; ?>    
               <h3><?php the_field('sous_titre'); ?></h3>
               <p><?php the_field('resume'); ?></p>
-              <a href="<?= get_permalink() ?>" class='bouton' style="text-align:center; font-weight:bold; font-size:1.2em">Voir plus</a></div>  
+              <a href="<?= get_permalink() ?>" class='bouton'>Voir plus</a></div>  
          
           <?php endwhile; ?>
       <?php endif; ?>  
   </div>  
+  <?php wp_nav_menu(array(
+        'menu' => 'menu accueil',
+        'container' => 'nav',
+        'menu' => 'menu atelier',
+    ));
+    ?>
 </section>
 <?php get_footer(); ?>
+
