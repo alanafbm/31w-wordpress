ae11936b14f556bd17915170788b9adedec6efa9
Alana Moraes <86125223+alanafbm@users.noreply.github.com>
2022-08-23 16:31:50 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 </section> <!-- fin .site -->
-<footer class="site__footer">
-<h2>Le pied de page</h2>
+<footer class="site__footer flex">
+<img src="https://www.cmaisonneuve.qc.ca/wp-content/uploads/2020/06/favicon-cm.jpg" width="100px" height="100px"></img>
+<h2>College Maisonneuve</h2>
+<p>3800, rue Sherbrooke Est</p>
+<p> Montréal (Québec)  H1X 2A2</p>
+<p>© 2022 Collège de Maisonneuve. Tous droits réservés.</p>
 </footer>
 <?php wp_footer(); ?>
 </body>
